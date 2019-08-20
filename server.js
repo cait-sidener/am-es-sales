630a189e14340657b129a1ea77bf240d555e0e18
Cait Sidener <cait.sidener@gmail.com>
2019-08-20 15:47:06 -0500

    styling and sizing

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ const _port = process.env.PORT || 4100;
 const app = express();
 app.use(compression());
 
-// app.get('*', function (req, res) {
-//   const index = path.join(__dirname, 'dist', _app_folder, 'index.html');  res.sendFile(index);
-// });
-
 // ---- SERVE STATIC FILES ---- //
 app.get('*.*', express.static(_app_folder, { maxAge: '1y' }));
 
