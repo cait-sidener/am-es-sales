e5d9651fc369845c5481c576efba2a6347b45c29
Cait Sidener <cait.sidener@gmail.com>
2019-08-20 13:38:23 -0500

    added path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(compression());
 
 app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'app/am-es-sales', 'index.html');
+  const index = path.join(__dirname, 'am-es-sales', 'index.html');
   res.sendFile(index);
  });
 
