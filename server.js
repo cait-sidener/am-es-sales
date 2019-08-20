b73816f46c7faf625578d3c97011a3b7a70fa39a
Cait Sidener <cait.sidener@gmail.com>
2019-08-20 14:18:52 -0500

    server app paths

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.get('*', function (req, res) {
   const index = path.join(__dirname, 'dist', 'am-es-sales', 'index.html');  res.sendFile(index);
 });
 
+// ---- SERVE STATIC FILES ---- //
+app.get('*.*', express.static(_app_folder, { maxAge: '1y' }));
+
+// ---- SERVE APLICATION PATHS ---- //
+app.all('*', function(req, res) {
+  res.status(200).sendFile(`/`, { root: path.join(__dirname, 'dist', 'am-es-sales') });
+  });
+
 // ---- START UP THE NODE SERVER  ----
 app.listen(_port, function() {
   console.log('Node Express server for ' + app.name + ' listening on http://localhost:' + _port);
